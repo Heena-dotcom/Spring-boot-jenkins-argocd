b880562160639eaadb22da57f3fc9a8e75966f8f
Heena-dotcom <heenabajariya94@gmail.com>
2025-02-10 21:14:56 -0500

    docker file change

--- spring-boot-app/src/main/java/com/example/spring_boot_app/SpringBootAppApplication.java
+++ spring-boot-app/src/main/java/com/example/spring_boot_app/SpringBootAppApplication.java
@@ -3,6 +3,7 @@ package com.example.spring_boot_app;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -17,7 +18,11 @@ public class SpringBootAppApplication {
 class HelloController {
     @GetMapping("/hello")
     public String sayHello() {
-        return "Hello, Spring Boot!";
+        return "Hello, from Spring Boot!";
+    }
+	@GetMapping("/hello/{name}")
+    public String greetUser(@PathVariable String name) {
+        return "Hello, " + name + "! Hope you're having a great day!";
     }
 }
 
